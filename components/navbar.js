d1d1d7f00eea30cb4063c3b94466a3992c84d619
unknown <anu250867anu.com>
2023-06-09 12:05:50 +0530

    improved csse

--- components/navbar.js
+++ components/navbar.js
@@ -3,24 +3,15 @@ function navbar() {
     <div id="logo">
       <a href="./index.html">
         <img
-          src="https://avatars.githubusercontent.com/u/9919?s=200&v=4"
-          alt=""
+          src="../assets/githubLogo.png"
+          alt="github logo"
         />
       </a>
     </div>
 
     <div id="search">
       <input type="text" placeholder="Search Users" id = "search-input" />
-      <button id="search-button">Search</button>
-    </div>
-
-    <div id="options">
-      <ul id="list">
-        <li>Pull Request</li>
-        <li>Issues</li>
-        <li>Marketplace</li>
-        <a href="./explore.html"><li>Explore</li></a>
-      </ul>
+      <button id="search-button"></button>
     </div>
   </div>`;
 }
--- scripts/getdata.js
+++ scripts/getdata.js
@@ -1,7 +1,6 @@
 async function getUser(user) {
   try {
     let res = await fetch(`https://api.github.com/search/users?q=${user}`);
-    // let res = await fetch(`https://api.github.com/users/${user}`);
     let data = await res.json();
     console.log("data:", data);
 
